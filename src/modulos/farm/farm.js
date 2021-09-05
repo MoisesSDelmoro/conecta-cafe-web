29164af2fd43ff15f0948c62890a678343742f6d
MDelmoro <mdelmoro@bedu.tech>
2021-09-05 18:17:46 -0300

    commit teste

--- src/modulos/farm/farm.js
+++ src/modulos/farm/farm.js
@@ -1,32 +1,43 @@
-import { Segment, Label } from "semantic-ui-react"
+import { Form, Segment, Label, Input, Button } from "semantic-ui-react"
 import React, { useEffect, useState } from "react";
 import './farm.css'
 import api from "../../services/api";
 
 const PageFarm = () => {
-  const [user, setUser] = useState();
+    const [user, setUser] = useState();
 
-  useEffect(() => {
-    api
-      .get("/farm/c43ecfab-69f0-46bd-aa38-4af3796350cf")
-      .then((response) => setUser(response.data))
-      .catch((err) => {
-        console.error("ops! ocorreu um erro" + err);
-      });
-  }, []);
+    useEffect(() => {
+        api
+            .get("/farm/c43ecfab-69f0-46bd-aa38-4af3796350cf")
+            .then((response) => setUser(response.data))
+            .catch((err) => {
+                console.error("ops! ocorreu um erro" + err);
+            });
+    }, []);
 
-  return (
+    return (
         <>
             <Segment className='page_farm'>
-                <Label  className='title_farm'>
+                <Form>
+                <Label className='title_farm'>
                     Fazenda
-                    Nome da fazenda: {user?.data.farm_name}
                 </Label>
+                    <Form.Field className='form'
+                        id='variedadef'
+                        label='variedade'
+                        placeholder='variedadef'
+                    />
+                    <Input placeholder='variedade' className='input_form' id='variedade' />
+                    <Input placeholder='altitude' className='input_form'/>
+                    <Input placeholder='colheita' className='input_form'/>
+
+                    <Input placeholder='valor na colheita' />
+                    Nome da fazenda: {user?.data.farm_name}
+                    <Button >ENVIAR</Button>
+                </Form>
             </Segment>
         </>
-   
-  
-  );
+    );
 }
 
 export default PageFarm
