0d05d2b0895862cce7932f9bdc940fc6f58dfcc1
MoisesSDelmoro <moisesdelmoro@gec.inatel.br>
2021-09-13 22:11:05 -0300

    ajustes pagefarm

--- src/modulos/farm/farm.js
+++ src/modulos/farm/farm.js
@@ -1,7 +1,7 @@
-import { Form, Segment, Label, Input, Button } from "semantic-ui-react"
+import { Segment, Label, Button, Input } from "semantic-ui-react"
+import { Template, FarmButton, FarmFields } from './styled';
 import React, { useEffect, useState } from "react";
 import api from "../../services/api";
-import { Template, FarmButton } from './styled';
 
 
 function PageFarm() {
@@ -16,39 +16,69 @@ function PageFarm() {
             });
     }, []);
 
+
+    const handleclickHistory = () => {
+        console.log("Cliquei Aqui em História!!!")
+    }
+
+    const handleclickPhotos = () => {
+        console.log("Cliquei Aqui em Fotos!!!")
+    }
+
+    const handleclickQRcode = () => {
+        console.log("Cliquei Aqui em QR Code!!!")
+    }
+
+    const handleclickLocation = () => {
+        console.log("Cliquei Aqui em Localização!!!")
+    }
+
+    const handleclickContact = () => {
+        console.log("Cliquei Aqui em Contato!!!")
+    }
+
     return (
         <>
             <Template>
                 <Segment className='page_farm'>
-                    <Form>
-                        <Label className='title_farm'>
-                            Fazenda
-                        </Label>
-                        <FarmButton>
-                            <Button className='option_button'>
-                                História
-                            </Button>
-                            <Button className='option_button'>
-                                Fotos/Vídeos
-                            </Button>
-                            <Button className='option_button'>
-                                QR Code
-                            </Button>
-                            <Button className='option_button'>
-                                Localização
-                            </Button>
-                            <Button className='option_button'>
-                                Contato
-                            </Button>
-                        </FarmButton>
-                        <Input placeholder='variedade' className='input_form' id='variedade' />
-                        <Input placeholder='altitude' className='input_form' />
-                        <Input placeholder='colheita' className='input_form' />
 
-                        <Input placeholder='valor na colheita' className='input_form' />
-                        Nome da fazenda: {user?.data.farm_name}
-                        <Button >ENVIAR</Button>
-                    </Form>
+                    <Label className='title_farm'>
+                        Fazenda
+                    </Label>
+                    <FarmButton>
+                        <Button className='option_button' onClick={handleclickHistory}>
+                            História
+                        </Button>
+                        <Button className='option_button' onClick={handleclickPhotos} >
+                            Fotos/Vídeos
+                        </Button>
+                        <Button className='option_button' onClick={handleclickQRcode}>
+                            QR Code
+                        </Button>
+                        <Button className='option_button' onClick={handleclickLocation}>
+                            Localização
+                        </Button>
+                        <Button className='option_button' onClick={handleclickContact}>
+                            Contato
+                        </Button>
+                    </FarmButton>
+
+                    <FarmFields>
+
+                        <Label className='sub_title_farm'>Contato</Label>
+
+
+                        <Label className='title_fields'>Telefone</Label>
+
+
+                        <Input placeholder='Entre com seu telefone' className='input_form' id='variedade' />
+
+                        <Input placeholder='Entre com seu e-mail' className='input_form' />
+
+                    </FarmFields>
+                    Nome da fazenda: {user?.data.farm_name}
+                    <Button >ENVIAR</Button>
+
                 </Segment>
             </Template>
         </>
--- src/modulos/farm/styled.js
+++ src/modulos/farm/styled.js
@@ -1,49 +1,82 @@
 import styled from 'styled-components';
 
-
-export const FarmButton = styled.div`
-    margin-top:2%;
-    height: 5vh;
-    width: 80vw;
-    background-color: #FF0000;
-    align-items: center;
-
-    .option_button{
-        align-items: center;
-        background: #FAF9F9;
-        border: 2px solid #6C6B6B;
-        border-radius: 10ch;
-        color: #6C6B6B;
-        font-size: calc(1.8vmin);
-        padding: 0.4vh 1.6vw;
-        margin-left: 20px;
-    }
-
-`;
-
 export const Template = styled.div`
     height: 80vh;
     width: 100vw;
 
     .page_farm {
-    margin-top:2%;
-    margin-left: 10%;
-    height: 80vh;
-    width: 80vw;
-    border-radius: 2ch;
-    color: rgb(255, 255, 255);
-    background-color: #FFFFFF;
-    font-size: calc(10px + 2vmin);
+        margin-top:2%;
+        margin-left: 10%;
+        height: 80vh;
+        width: 80vw;
+        border-radius: 2ch;
+        color: rgb(255, 255, 255);
+        background-color: #FFFFFF;
+        font-size: calc(10px + 2vmin);
     }
 
     .title_farm{
-    text-align: center;
-    margin-top: 1vh;
-    color: rgb(66, 124, 81);
+        text-align: center;
+        margin-top: 1vh;
+        color: rgb(66, 124, 81);
+        font-size: calc(5vmin);
+
+}
+`;
+
+export const FarmButton = styled.div`
+    margin-top:2%;
+    margin-left: 20%;
+    height: 5vh;
+    width: 80vw;
+    align-items: center;
+
+    .option_button{
+        align-items: center;
+        background: #EEFBDF;
+        border: 2px solid #EEFBDF;
+        border-radius: 10ch;
+        color: #4B9100;
+        font-size: calc(1.8vmin);
+        padding: 0.4vh 1.6vw;
+        margin-left: 20px;
+    }
+`;
+
+export const FarmFields = styled.div`
+    height: 58vh;
+    width: 80vw;
+    margin-top: 4%;
+    align-items: center;
+    
+    .sub_title_farm{
+        text-align: left;
+        margin-left: 10%;
+        margin-top: 1vh;
+        color: #394B68;
+        font-size: calc(3vmin);
+        font-style: bold;
+    }
 
     .input_form{
+        margin-top: 2vh;
+        margin-left: 12%;
         align-items: center;
-        background-color: #FF0000;
+        background: #EEFBDF;
+        border: 2px solid #EEFBDF;
+        border-radius: 10ch;
+        color: #4B9100;
+        font-size: calc(1.8vmin);
+        padding: 0.4vh 1.6vw;
+        margin-left: 20px;
     }
-}
+
+    .title_fields{
+        text-align: left;
+        margin-top: 2vh;
+        margin-left: 12%;
+        color: #706D6D;
+        font-size: calc(1.8vmin);
+        font-style: bold;
+    }
 `;
